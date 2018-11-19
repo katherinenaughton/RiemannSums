a87a73e196f073c6d8ccb18048d5aa3eceb9c5d0
katherinenaughton <42843645+katherinenaughton@users.noreply.github.com>
2018-11-19 17:47:23 -0500

    Updated from Brython Server: 11/19/2018 5:47:21 PM

--- .py
+++ .py
@@ -1,4 +1,3 @@
-
 function=input("Please input a function: ")
 x1=int(input("Where would you like your interval to start? "))
 x2=int(input("Where would you like your interval to end? "))
@@ -6,15 +5,36 @@ rectangles=int(input("How many rectangles would you like to have? "))
 
 width=(x2-x1)/rectangles
 
-xcoord=[]
 
-ycoordlist=[]                               #This prints a list of the y values. 
-for r in range(x1, x2+1):
+ycoordlistL=[]                               #This prints a list of the y values. 
+for r in range(x1, x2):
     x=r
     Locfunction=function.lower()
     y=eval(Locfunction)
     ycoordlist.append(y)
-#print(ycoordlist)
+print(ycoordlist)
+
+ycoordlistR=[]                               #This prints a list of the y values. 
+for r in range(x1-1, x2+1):
+    x=r
+    Locfunction=function.lower()
+    y=eval(Locfunction)
+    ycoordlist.append(y)
+print(ycoordlist)
+
+xcoordlistM=[]
+for r in range (x1, x2):
+    x=r+(width/2)
+    xcoordlistM.append(x)
+print(xcoordlistM)
+
+ycoordlistM=[]                               
+for r in xcoordlistM:
+    x=r
+    Locfunction=function.lower()
+    y=eval(Locfunction)
+    ycoordlist.append(y)
+print(ycoordlist)
 
 arealist=[]
 for r in range(x1, x2+1):
@@ -24,8 +44,6 @@ print(arealist)
 
 sum=sum(arealist)
 print(sum)
-    
-
 
 
 
