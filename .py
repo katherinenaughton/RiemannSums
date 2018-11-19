7dec86a60e0e4c0cd50755b019846949b8061f26
katherinenaughton <42843645+katherinenaughton@users.noreply.github.com>
2018-11-19 08:45:49 -0500

    Updated from Brython Server: 11/19/2018 8:45:48 AM

--- .py
+++ .py
@@ -1,1 +1,24 @@
 
+function=input("Please input a function: ")
+x1=int(input("Where would you like your interval to start? "))
+x2=int(input("Where would you like your interval to end? "))
+rectangles=int(input("How many rectangles would you like to have? "))
+
+width=(x2-x1)/rectangles
+
+xcoord=[]
+
+ycoordlist=[]                               #This prints a list of the y values. 
+for r in range(x1, x2+1):
+    x=r
+    Locfunction=function.lower()
+    y=eval(Locfunction)
+    ycoordlist.append(y)
+#print(ycoordlist)
+
+for r in range 
+
+
+
+
+
