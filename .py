ff8cd6ffba2453f4d075279575ce8e980515c215
katherinenaughton <42843645+katherinenaughton@users.noreply.github.com>
2018-11-20 08:29:03 -0500

    Updated from Brython Server: 11/20/2018 8:29:02 AM

--- .py
+++ .py
@@ -1,6 +1,3 @@
-from math import sin, cos, tan, acos, asin, atan
-from math import exp, e, pi
-from math import log, log10, sqrt, log2
 function=input("Please input a function: ")
 x1=int(input("Where would you like your interval to start? "))
 x2=int(input("Where would you like your interval to end? "))
@@ -9,15 +6,6 @@ rectangles=int(input("How many rectangles would you like to have? "))
 width=((x2-x1)/rectangles)
 print(width)
 
-function=input("Please input a function: ")
-x1=int(input("Where would you like your interval to start? "))
-x2=int(input("Where would you like your interval to end? "))
-rectangles=int(input("How many rectangles would you like to have? "))
-
-width=((x2-x1)/rectangles)
-print(width)
-
-a = x2-x1
 
 xcoordlist=[]
 b=0
@@ -26,6 +14,29 @@ for i in range(rectangles+1):
     b+=1
 
 print("your x=coordinates are: ",xcoordlist)
+
+LRAMxcoord = []
+RRAMxcoord = []
+MRAMxcoord = []
+
+b=0
+for i in range(len(xcoordlist)-1):
+    LRAMxcoord.append(xcoordlist[i])
+
+b=1   
+for i in xcoordlist:
+    s = xcoordlist[b]
+    MRAMxcoord.append(i+s/2)
+
+Rxcoordlist = xcoordlist[::-1]  
+for i in range(len(xcoordlist)-1):
+    RRAMxcoord.append(Rxcoordlist[i])
+ 
+
+print(LRAMxcoord)
+print(RRAMxcoord)
+print(MRAMxcoord)
+
 
 lastxcoord=xcoordlist[-1]
 
@@ -91,69 +102,5 @@ print(Rsum)
 
 Msum=sum(Marealist)
 print(Msum)
-
-
-
-
-
-ycoordlistL=[]                               #This prints a list of the y values. 
-for r in range(x1, x2):
-    x=r
-    Locfunction=function.lower()
-    y=eval(Locfunction)
-    ycoordlistL.append(y)
-print(ycoordlistL)
-
-ycoordlistR=[]                               #This prints a list of the y values. 
-for r in range(x1-1, x2+1):
-    x=r
-    Locfunction=function.lower()
-    y=eval(Locfunction)
-    ycoordlistR.append(y)
-print(ycoordlistR)
-
-xcoordlistM=[]
-for r in range (x1, x2):
-    x=r+(width/2)
-    xcoordlistM.append(x)
-print(xcoordlistM)
-
-ycoordlistM=[]                               
-for r in xcoordlistM:
-    x=r
-    Locfunction=function.lower()
-    y=eval(Locfunction)
-    ycoordlistM.append(y)
-print(ycoordlistM)
-
-length=len(ycoordlistL)
-Larealist=[]
-for r in range(length):
-    area=ycoordlistL[r]*width
-    Larealist.append(area)
-print(Larealist)
-
-Rarealist=[]
-for r in range(length):
-    area=ycoordlistR[r]*width
-    Rarealist.append(area)
-print(Rarealist)
-
-Marealist=[]
-for r in range(length):
-    area=ycoordlistM[r]*width
-    Marealist.append(area)
-print(Marealist)
-
-Lsum=sum(Marealist)
-print(Lsum)
-
-Rsum=sum(Rarealist)
-print(Rsum)
-
-Msum=sum(Marealist)
-print(Msum)
-
-
 
 
